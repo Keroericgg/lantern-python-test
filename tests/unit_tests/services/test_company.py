726a3c548cb1becf835d58d0accb24a147867c86
ericleung <leungchungwaieric1996@gmail.com>
2024-06-03 00:01:47 +0100

    small refactoring

--- tests/unit_tests/services/test_company.py
+++ tests/unit_tests/services/test_company.py
@@ -6,11 +6,11 @@ from unittest.mock import patch, Mock
 from tests.fixtures.company_info import CompanyInfoFromStoreFixtures, CompanyInfoFromPdfFixtures
 
 
+@patch("src.services.company.get_company_info_from_store")
 class TestCompanyServiceGetCompanyInfo(unittest.TestCase):
     def setUp(self):
         self.service = CompanyService("foo", "boo")
 
-    @patch("src.services.company.get_company_info_from_store")
     def test_get_company_info(self, mock_get_company_info_from_store: Mock):
         mock_get_company_info_from_store.return_value = "foo"
 
@@ -18,11 +18,11 @@ class TestCompanyServiceGetCompanyInfo(unittest.TestCase):
         mock_get_company_info_from_store.assert_called_once()
 
 
+@patch("src.services.company.CompanyService.get_company_info")
 class TestCompanyServiceCheckDiscrepancy(unittest.TestCase):
     def setUp(self):
         self.service = CompanyService("foo", "boo")
 
-    @patch("src.services.company.CompanyService.get_company_info")
     def test_check_discrepancy_all_matching(self, mock_get_company_info: Mock):
         mock_get_company_info.return_value = CompanyInfoFromStoreFixtures[0]
         discrepancy_response = self.service.check_discrepancy(CompanyInfoFromStoreFixtures[0])
@@ -30,7 +30,6 @@ class TestCompanyServiceCheckDiscrepancy(unittest.TestCase):
         mock_get_company_info.assert_called_once()
         self.assertNotIn(True, discrepancy_response.discrepancy.model_fields.values())
 
-    @patch("src.services.company.CompanyService.get_company_info")
     def test_check_discrepancy_with_discrepancy(self, mock_get_company_info: Mock):
         mock_get_company_info.return_value = CompanyInfoFromStoreFixtures[0]
         discrepancy_response = self.service.check_discrepancy(CompanyInfoFromPdfFixtures[0])
--- tests/unit_tests/store/test_get_company_info_from_store.py
+++ tests/unit_tests/store/test_get_company_info_from_store.py
@@ -1,34 +1,35 @@
 import unittest
-
 from unittest.mock import patch, Mock
 
+from pydantic import ValidationError
+
+from src.logger import logger
+from src.store.db import DB
 from src.store.get_company_info_from_store import get_company_info_from_store
 from tests.fixtures.company_info import CompanyInfoFromStoreFixtures, CompanyDataStoreFixtures
 
 
+@patch.object(logger, "error")
+@patch.object(DB, "find")
 class TestGetCompanyInfoFromStore(unittest.TestCase):
-    @patch("csv.DictReader")
-    def test_get_company_info_from_store_valid_company_name(self, mock_csv_reader: Mock):
-        mock_csv_reader.return_value = [CompanyDataStoreFixtures[0]]
+    def test_get_company_info_from_store_valid_company_name(self, mock_db_find: Mock, mock_error_logger: Mock):
+        mock_db_find.return_value = [CompanyDataStoreFixtures[0]]
 
         self.assertEqual(CompanyInfoFromStoreFixtures[0], get_company_info_from_store("HealthInc"))
-        mock_csv_reader.assert_called_once()
+        mock_db_find.assert_called_once()
+        mock_error_logger.assert_not_called()
 
-    @patch("src.store.get_company_info_from_store.logger.error")
-    @patch("csv.DictReader")
-    def test_get_company_info_from_store_no_company_found(self, mock_csv_reader: Mock, mock_error_logger: Mock):
-        mock_csv_reader.return_value = []
+    def test_get_company_info_from_store_no_company_found(self, mock_db_find: Mock, mock_error_logger: Mock):
+        mock_db_find.return_value = []
 
         self.assertRaises(ValueError, get_company_info_from_store, "foo")
-        mock_csv_reader.assert_called_once()
+        mock_db_find.assert_called_once()
         mock_error_logger.assert_called_once_with(
             "No data returned by the given company name", extra={"data": {"number_of_record": 0}}
         )
 
-    @patch("src.store.get_company_info_from_store.logger.error")
-    @patch("csv.DictReader")
-    def test_get_company_info_from_store_more_than_one_record(self, mock_csv_reader: Mock, mock_error_logger: Mock):
-        mock_csv_reader.return_value = [
+    def test_get_company_info_from_store_more_than_one_record(self, mock_db_find: Mock, mock_error_logger: Mock):
+        mock_db_find.return_value = [
             CompanyDataStoreFixtures[0],
             {
                 **CompanyDataStoreFixtures[1],
@@ -37,10 +38,17 @@ class TestGetCompanyInfoFromStore(unittest.TestCase):
         ]
 
         self.assertRaises(ValueError, get_company_info_from_store, "HealthInc")
-        mock_csv_reader.assert_called_once()
+        mock_db_find.assert_called_once()
         mock_error_logger.assert_called_once_with(
             "More than 1 record of data returned by the given company name", extra={"data": {"number_of_record": 2}}
         )
+
+    def test_get_company_info_from_store_validation_error(self, mock_db_find: Mock, mock_error_logger: Mock):
+        mock_db_find.return_value = [{"foo": "boo"}]
+
+        self.assertRaises(ValidationError, get_company_info_from_store, "HealthInc")
+        mock_db_find.assert_called_once()
+        mock_error_logger.assert_not_called()
 
 
 if __name__ == '__main__':
